d262344e7cbfea98e9de25c878bced2a038dd506
Dev <flashytesting@gmail.com>
2022-01-27 15:51:44 +0530

    Add hero animation

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+gsap.from("header nav ul .left", {
+  opacity: 0,
+  duration: 1,
+  x: 50,
+  delay: 2.55,
+});
+gsap.from("header nav ul .center", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.55,
+});
+gsap.from("header nav ul .right", {
+  opacity: 0,
+  duration: 1,
+  x: -50,
+  delay: 2.55,
+});
+
+gsap.from(".hero .hero-box .content .title", {
+  opacity: 0,
+  duration: 1,
+  y: -30,
+  delay: 0.5,
+});
+gsap.from(".hero .hero-box .content .description", {
+  opacity: 0,
+  duration: 1.5,
+  y: 30,
+  delay: 1,
+});
+gsap.from(".hero .hero-box .content .btn", {
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+});
+
 const sliders = document.querySelectorAll(".slide");
 const options = { rootMargin: "-150px" };
 
@@ -14,4 +50,22 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 sliders.forEach((slide) => {
   observer.observe(slide);
+});
+
+const dividers = document.querySelectorAll(".divider");
+const divOptions = { rootMargin: "-20px" };
+
+const onScroll = new IntersectionObserver((entries, onScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("show-div");
+      onScroll.unobserve(entry.target);
+    }
+  });
+}, divOptions);
+
+dividers.forEach((divider) => {
+  onScroll.observe(divider);
 });
